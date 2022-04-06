cb1a8408e44d7ad2411d7d2fb2484eb4cfd8fb0a
Maxime Tournay <mtournay@fu-r4-p4.s19.be>
2022-04-06 18:26:32 +0200

    l

--- builtins/unset.c
+++ builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: mtournay <mtournay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 16:20:41 by mtournay          #+#    #+#             */
-/*   Updated: 2022/04/05 16:26:42 by mtournay         ###   ########.fr       */
+/*   Updated: 2022/04/06 18:25:42 by mtournay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,55 @@
 #include "builtins.h"
 #include "exec.h"
 
-static int	unset_2(char ***env, int i, char **ret)
+static int	comp(char *s1, char *s2, int n)
 {
-	if (!(*env)[i])
-	{
-		free(ret);
+	if (!s1 || !s2)
 		return (1);
-	}
-	i++;
-	free((*env)[i -1]);
-	while ((*env)[i])
+	while (n--)
 	{
-		ret[i - 1] = (*env)[i];
-		i++;
+		if (*s1 != *s2 && !*s2 && *s1 == '=')
+			return (0);
+		if (*s1 != *s2)
+			return (*(unsigned char *)s1 - *(unsigned char *)s2);
+		if (!*s1)
+			return (1);
+		s1++;
+		s2++;
 	}
-	ret[i - 1] = NULL;
-	free(*env);
-	*env = ret;
 	return (1);
+}
+
+static void	ret_malloc(char ***ret, int i)
+{
+	*ret = malloc(sizeof(char *) * i);
+	if (!(*ret))
+		return (exit(0));
 }
 
 int	ft_unset(char ***env, char **cmd)
 {
 	int		i;
+	int		j;
 	char	**ret;
 
-	i = 0;
-	while ((*env)[i])
-		i++;
-	ret = malloc(sizeof(char *) * i);
-	if (!ret)
-		return (exit(0), 1);
-	i = 0;
-	while (cmp((*env)[i], cmd[1], ft_strlen(cmd[1])) && (*env)[i])
+	j = 0;
+	while (cmd[++j])
 	{
-		ret[i] = (*env)[i];
-		i++;
+		i = 0;
+		while ((*env)[i])
+			i++;
+		ret_malloc(&ret, i);
+		i = -1;
+		while (comp((*env)[++i], cmd[j], ft_strlen(cmd[1]) + 1) && (*env)[i])
+			ret[i] = (*env)[i];
+		if (!(*env)[i])
+			return (free(ret), 1);
+		free((*env)[i]);
+		while ((*env)[++i])
+			ret[i - 1] = (*env)[i];
+		ret[i - 1] = NULL;
+		free(*env);
+		*env = ret;
 	}
-	return (unset_2(env, i, ret));
+	return (1);
 }
